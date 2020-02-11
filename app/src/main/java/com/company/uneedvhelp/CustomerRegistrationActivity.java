f11922a9001ee42cda1a0fdd5217b14fae78b89e
Tung Pham <tp2503@lanvera.org>
2020-02-11 02:42:27 -0600

    add simple registration

---
+++ app/src/main/java/com/company/uneedvhelp/CustomerRegistrationActivity.java
@@ -0,0 +1,117 @@
+package com.company.uneedvhelp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.iid.Registrar;
+
+public class CustomerRegistrationActivity extends AppCompatActivity {
+    EditText mFirstName, mLastName, mEmail, mPassword, mConfirmPassword, mPhone;
+    Button mRegisterBtn;
+    TextView mLoginBtn;
+    ProgressBar mProgressBar;
+    FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_registration);
+
+        mFirstName = findViewById(R.id.firstName);
+        mLastName = findViewById(R.id.lastName);
+        mEmail = findViewById(R.id.email);
+        mPassword = findViewById(R.id.password);
+        mConfirmPassword = findViewById(R.id.confirm_pw);
+        mPhone = findViewById(R.id.phone);
+        mRegisterBtn = findViewById(R.id.signup_button);
+        mLoginBtn =  findViewById(R.id.login);
+        mProgressBar = findViewById(R.id.progressBar);
+        mAuth = FirebaseAuth.getInstance();
+
+        if(mAuth.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }
+
+        mRegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String firstName = mFirstName.getText().toString().trim();
+                String lastName = mLastName.getText().toString().trim();
+                String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString();
+                String confirmPassword = mConfirmPassword.getText().toString();
+                String phone = mPhone.getText().toString().trim();
+
+                if(TextUtils.isEmpty(firstName)){
+                    mFirstName.setError("First Name is Required");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(lastName)){
+                    mLastName.setError("Last Name is Required");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(phone)){
+                    mPhone.setError("Phone Number is Required");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(email)){
+                    mEmail.setError("Email is Required");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)){
+                    mPassword.setError("Please Enter Your Password");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(confirmPassword)){
+                    mConfirmPassword.setError("Please Enter Confirm Password");
+                    return;
+                }
+
+                if(password.length() < 6){
+                    mPassword.setError("Password must be longer than 6 characters");
+                    return;
+                }
+
+                if(!password.equals(confirmPassword)){
+                    mConfirmPassword.setError("Your Confirm Password does not match");
+                    return;
+                }
+
+                mProgressBar.setVisibility(View.VISIBLE);
+
+                mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(CustomerRegistrationActivity.this, "You are Signed Up. ", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        } else{
+                            Toast.makeText(CustomerRegistrationActivity.this, "Error! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+    }
+}
